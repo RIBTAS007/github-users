964290995e4bc1c534a01ce84dee9df88da56367
Ribtas007 <ribtas007@outlook.com>
2024-02-14 21:02:46 +0530

    completed till search functionality

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Chart from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
-const Pie3D = () => {
-  return <div>chart</div>;
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const Pie3D = ({ data }) => {
+  const chartConfigs = {
+    type: "pie3d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Languages]",
+        theme: "fusion",
+        decimals: 0,
+        pieRadius: "45%",
+        // paletteColors:'#f0db4f, #FFFFFF '
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Pie3D;
